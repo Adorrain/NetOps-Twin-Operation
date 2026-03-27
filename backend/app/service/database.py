cc389e352f977c9b529e7550421780bc83914c2e
Adorrain <lh0115zyf@163.com>
2026-03-27 14:08:24 +0800

    fix postgresql serects

--- backend/app/service/database.py
+++ backend/app/service/database.py
@@ -6,11 +6,11 @@ from sqlalchemy.orm import sessionmaker
 from flask import g
 import os
 
-DB_HOST = os.getenv("DB_HOST", "47.95.157.92")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "netops")
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "123456")
+DB_HOST = os.getenv("DB_HOST", "")
+DB_PORT = os.getenv("DB_PORT", "")
+DB_NAME = os.getenv("DB_NAME", "")
+DB_USER = os.getenv("DB_USER", "")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 
 
 
