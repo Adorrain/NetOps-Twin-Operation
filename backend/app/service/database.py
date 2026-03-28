695bf73d6c3f06720dd9006638ba8b81286d32fa
Adorrain <lh0115zyf@163.com>
2026-03-28 22:07:31 +0800

    fix some issues

--- backend/app/service/database.py
+++ backend/app/service/database.py
@@ -6,11 +6,11 @@ from sqlalchemy.orm import sessionmaker
 from flask import g
 import os
 
-DB_HOST = os.getenv("DB_HOST", "47.95.157.92")
-DB_PORT = os.getenv("DB_PORT", "5439")
-DB_NAME = os.getenv("DB_NAME", "netops")
-DB_USER = os.getenv("DB_USER", "netops_user")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "jiangke20260328")
+DB_HOST = os.getenv("DB_HOST", "")
+DB_PORT = os.getenv("DB_PORT", "")
+DB_NAME = os.getenv("DB_NAME", "")
+DB_USER = os.getenv("DB_USER", "")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 
 
 
